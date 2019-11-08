8ff77d94461a2652694c4665cdcd6674da28c7e5
valya-tarasenko <57531416+valya-tarasenko@users.noreply.github.com>
2019-11-08 16:29:59 +0300

    Add files via upload
    add task begin25

--- begin-25.pas
+++ begin-25.pas
@@ -1,4 +1,12 @@
+﻿//
+//Begin25◦
+//Найти значение функции y = 3x^6 − 6x^2 − 7 при данном значении x.
+//
 program begin25;
+var
+  x, y: real;
 begin 
-  
+  readln(x);
+  y := 3 * power(x, 6) - 6 * power(x, 2) - 7;
+  writeln(y);
 end.
