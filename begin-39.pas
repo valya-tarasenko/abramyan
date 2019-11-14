24148dc1ce1f497b320d62b332599747ed5c1ad4
valya-tarasenko <57531416+valya-tarasenko@users.noreply.github.com>
2019-11-14 16:28:45 +0300

    Add files via upload
    add begin-39

---
+++ begin-39.pas
@@ -0,0 +1,10 @@
+﻿program begin39;
+var
+  a, b, c, x1, x2, d: real;
+begin
+  readln(a, b, c);
+  d := sqrt(power(b, 2) - 4 * a * c);
+  x1 := (-b - d) / (2 * a);
+  x2 := (-b + d) / (2 * a);
+  writeln(x1, ' ', x2, ' ');
+end.
